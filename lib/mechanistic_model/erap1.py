3b537433f2ca282f9c82ce63c7837c7b473bd0d3
Miles Weatherseed <weatherseed@maths.ox.ac.uk>
2024-09-30 23:43:13 +0100

    Cleaning up utils, cyt amin and erap1

---
+++ lib/mechanistic_model/erap1.py
@@ -0,0 +1,214 @@
+import numpy as np
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVR
+from utils.utils import (
+    make_length,
+    sparse_peptide_encoding,
+    VALID_ENCODINGS,
+    physico_peptide_encoding,
+    submatrix_peptide_encoding,
+)
+import joblib
+import os
+
+PAD_START = 5
+PAD_LENGTH = 11
+
+# slope to convert regressor output to kcats - from M_Weatherseed PhD thesis.
+SLOPE = 0.0400
+
+# best performing encodings and corresponding hyperparameters from M_Weatherseed PhD thesis
+encodings = ["benner74", "blosum50", "feng", "gonnet1992", "levin"]
+Cs = [
+    1.0669882710369707,
+    6.935400690096964,
+    96.8120891912843,
+    1.386378561590795,
+    58.47739859832214,
+]
+epsilons = [
+    0.0039493644472223316,
+    0.22594607000759603,
+    0.0016649059071639674,
+    0.015500706089539403,
+    0.17504766488065068,
+]
+kernels = ["linear", "linear", "rbf", "linear", "rbf"]
+
+
+def generate_datasets_erap(
+    peptides: np.ndarray, startpoint: int, encoding: str, length: int
+) -> np.ndarray:
+    """_summary_
+
+    Generates the datasets required for training the ERAP1 predictor, based on
+    user desired encoding and padding strategies.
+    Args:
+        peptides (np.ndarray): Array of peptide strings in the training data.
+        startpoint (int): Which point in the peptide to start padding or trimming (N.B. corresponds to P_{i+1} due to Python's indexing on 0)
+        encoding (str): Which encoding method to use (e.g. "BLOSUM62")
+        length (int): The length to pad/trim all peptides to
+
+    Returns:
+        np.ndarray: array of encoded and length-normalised peptide representations.
+    """
+
+    normalised_peptides = make_length(peptides, startpoint, length)
+
+    if encoding not in VALID_ENCODINGS:
+        raise ValueError("Must choose a method in VALID_ENCODINGS")
+
+    if encoding == "sparse":
+        train_peptide_encodings = sparse_peptide_encoding(normalised_peptides)
+    elif encoding == "physico":
+        train_peptide_encodings = physico_peptide_encoding(normalised_peptides)
+    else:
+        train_peptide_encodings = submatrix_peptide_encoding(
+            normalised_peptides, encoding
+        )
+    return train_peptide_encodings
+
+
+def generate_ensemble_models():
+    """
+    This function should be called when POEM is installed on a new machine,
+    creating new versions of the regressors and avoiding and scikit-learn
+    compatability issues
+    """
+    # load training data
+    data = pd.read_csv("lib/mechanistic_model/data/erap1_training_data.csv")
+    # count number of times each peptide appears in data
+    data["count"] = [
+        sum(data.peptide.values == p) for p in data.peptide.values
+    ]
+    # we weight the training data inversely by number of appearances of each peptide
+    weights = 1 / data["count"].values
+    # we log scale and truncate at ~1% trimming rate per hour
+    y = np.log10(data.trimming_rate.values)
+    y[y <= -0.5] = -0.5
+    peptides = data.peptide.values
+    # create ensemble
+    trained_models = train_models(
+        weights, peptides, y, encodings, Cs, epsilons
+    )
+    # save these into a cache for easy loading later
+    for enc, model in zip(encodings, trained_models):
+        joblib.dump(
+            model,
+            open(f"lib/mechanistic_model/cache/erap1_models/{enc}.reg", "wb"),
+            protocol=5,
+        )
+    return
+
+
+def train_models(
+    sample_weights: np.ndarray,
+    peptides: np.ndarray,
+    y: np.ndarray,
+    encodings: list[str],
+    Cs: list[float],
+    epsilons: list[float],
+) -> list:
+    """Re-trains models and groups together so that we don't run into any sklearn version issues
+
+    Args:
+        sample_weights (np.ndarray): 1/peptide_count, where peptide_count is the number of times the peptide is repeated in the training data
+        peptides (np.ndarray): _description_
+        y (np.ndarray): log10 of the trimming rate
+        encodings (list[str]): list of amino acid encodings in the ensemble
+        Cs (list[float]): list of SVR regularisation terms in the ensemble
+        epsilons (list[float]): list of epsilons for softmargin lost function
+
+    Returns:
+        list: list of trained regressors in ensemble
+    """
+    models = []
+
+    for i, enc in enumerate(encodings):
+        pipeline = Pipeline(
+            [
+                ("scaler", MinMaxScaler()),
+                (
+                    "regressor",
+                    SVR(
+                        kernel=kernels[i],
+                        C=Cs[i],
+                        epsilon=epsilons[i],
+                        cache_size=1600,
+                        max_iter=1000000000,
+                    ),
+                ),
+            ]
+        )
+        X = generate_datasets_erap(
+            peptides,
+            startpoint=PAD_START,
+            encoding=enc,
+            length=PAD_LENGTH,
+        )
+        pipeline.fit(X, y, regressor__sample_weight=sample_weights)
+        models.append(pipeline)
+
+    return models
+
+
+# if cache of ensemble models doesn't exist, we should re-train it
+if any(
+    [
+        not os.path.exists(
+            f"lib/mechanistic_model/cache/erap1_models/{enc}.reg"
+        )
+        for enc in encodings
+    ]
+):
+    generate_ensemble_models()
+
+models = [
+    joblib.load(
+        open(f"lib/mechanistic_model/cache/erap1_models/{enc}.reg", "rb")
+    )
+    for enc in encodings
+]
+
+
+def predict_scores(peptides: np.ndarray) -> np.ndarray:
+    """Runs each regressor in ensemble and returns arithmetic mean of their scores
+
+    Args:
+        peptides (np.ndarray): Array of peptides to form predictions for
+
+    Returns:
+        np.ndarray: A score for each peptide, corresponding to the expected
+        rate of trimming by ERAP1 in specific assay conditions (defined in M_Weatherseed thesis)
+    """
+
+    predictions = np.zeros((len(peptides), len(encodings)))
+    for i in range(len(encodings)):
+        X = generate_datasets_erap(
+            peptides,
+            PAD_START,
+            encodings[i],
+            PAD_LENGTH,
+        )
+        predictions[:, i] = models[i].predict(X)
+
+    return np.mean(predictions, axis=1)
+
+
+def get_kcats(peptides: np.ndarray) -> np.ndarray:
+    """Takes in array of peptides and returns an array of the predicted kcats (in s^-1)
+
+    Args:
+        peptides (np.ndarray): Peptide sequences to make predictions on
+
+    Returns:
+        np.ndarray: predicted kcats (in s^-1)
+    """
+    return SLOPE * 10 ** (predict_scores(peptides))
+
+
+if __name__ == "__main__":
+    # write this into tests eventually - check you get known output
+    print(get_kcats(np.array(["SIINFEKLM", "SIINYEKL"])))
---
+++ lib/mechanistic_model/utils/utils.py
@@ -0,0 +1,348 @@
+import numpy as np
+import pandas as pd
+from Bio.Align import substitution_matrices
+import json
+import mhcgnomes
+
+# load substitution matrices, define AAs and load in physicochemical features
+SUBS_MATRICES = substitution_matrices.load()
+AMINO_ACIDS = [
+    "A",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "K",
+    "L",
+    "M",
+    "N",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "V",
+    "W",
+    "Y",
+    "X",
+]  # canonical amino acids
+PHYSICO_DICT = json.load(
+    open("lib/mechanistic_model/utils/aaindex1_pca.json", "r")
+)
+VALID_ENCODINGS = [x.lower() for x in SUBS_MATRICES] + [
+    "physico",
+    "sparse",
+]
+HUMAN_TAP_PSEUDOSEQ = "SSSERCTMNPLVRARMTI"
+MOUSE_TAP_PSEUDOSEQ = "CSVQSSTMRPFIKDRINM"
+RAT_A_TAP_PSEUDOSEQ = "CNVESSAEQSLIKSQINM"
+RAT_U_TAP_PSEUDOSEQ = "CNVESSTMRPFIKERINM"
+
+# PEPTIDE HANDLING FUNCTIONS
+
+
+# Functions facilitating general manipulation of peptide sequences to vectors
+
+
+def sparse_peptide_encoding(peptides: list) -> np.ndarray:
+    """_summary_
+    Takes a peptide list input and returns a sparse representation of the peptides.
+    Args:
+        peptide (list): Peptides to be encoded
+    """
+    lengths = set([len(x) for x in peptides])
+    assert len(lengths) == 1  # can't handle mixed lengths
+    output = np.zeros((len(peptides), 21 * len(peptides[0])))
+    for i in range(len(peptides)):
+        for idx, s in enumerate(peptides[i]):
+            output[i, idx * 21 + AMINO_ACIDS.index(s)] += 1
+    return np.array(output)
+
+
+def physico_peptide_encoding(peptides: list) -> np.ndarray:
+    """_summary_
+
+    Args:
+        peptides (list): List of peptides to encode using length 19 PCA decomposition of AAIndex features
+    """
+    outputs = np.zeros((len(peptides), len(peptides[0]) * 19))
+    for i in range(len(peptides)):
+        outputs[i, :] = np.array(
+            [PHYSICO_DICT[s] for s in peptides[i]]
+        ).flatten()
+    return outputs
+
+
+def submatrix_peptide_encoding(peptides: list, matrix: str) -> np.ndarray:
+    """_summary_
+    Converts a list of peptides to substitution matrix encodings.
+    Args:
+        peptide (list): List of peptides to be encoded
+        matrix (str): The substitution matrix name to use (e.g. "BLOSUM62")
+    """
+    assert matrix.upper() in SUBS_MATRICES
+    M = substitution_matrices.load(name=matrix.upper())
+
+    # some of the substitution matrices in the Biopython package don't have the X amino acid
+    # TODO: decide on better way to handle these. Replace with alanine for now.
+
+    if ("X", "X") not in M.keys():
+        for i in range(len(peptides)):
+            peptides[i] = peptides[i].replace(
+                "X", "A"
+            )  # switch to alanine padding for now
+    return np.array(
+        [np.array([M[x].values() for x in p]).flatten() for p in peptides]
+    )
+
+
+def pad_peptide_tolength(peptide: str, pad_start: int, length: int) -> str:
+    """
+    This function takes in a peptide and pads with the unknown amino acid
+    X, starting at site pad_start.
+    """
+    return (
+        peptide[:pad_start]
+        + "X" * (length - len(peptide))
+        + peptide[pad_start:]
+    )
+
+
+def trim_peptide_tolength(peptide: str, trim_start: int, length: int) -> str:
+    """
+    This function takes in a list of peptides of different lengths and
+    converts it to a list of nmers by trimming internal amino acids, starting
+    at site trim_start.
+    """
+    return peptide[:trim_start] + peptide[-(length - trim_start) :]
+
+
+def make_length(peptides: np.ndarray, start: int, length: int) -> list:
+    """Takes in peptides and converts them all to the same length by a mixture
+    of padding and trimming
+
+    Args:
+        peptides (np.ndarray): Array of peptides to be normalised.
+        start (int): Point at which to start trimming or padding.
+        length (int): The length to normalise all peptides to.
+
+    Returns:
+        list: _description_
+    """
+    output = list(peptides)
+    output = [
+        pad_peptide_tolength(x, start, length) if len(x) < length else x
+        for x in output
+    ]
+    output = [
+        trim_peptide_tolength(x, start, length) if len(x) > length else x
+        for x in output
+    ]
+    return output
+
+
+# FUNCTIONS TO HANDLE LARGE PROTEINS
+
+
+def find_peptide_precursors(
+    length: int, fasta_sequence: str, lengths: list, start_position: int
+):
+    end_position = start_position + length - 1
+    peptides = [
+        "A" * max(-(end_position - x + 1), 0)
+        + fasta_sequence[max(end_position - x + 1, 0) : end_position + 1]
+        for x in lengths
+    ]  # pre-pad with alanine if needed - the proteasome model will capture that this doesn't exist
+    return peptides
+
+
+def protein_to_peptides(protein_sequence: str):
+    # this returns a list of startpoint and list of endpoints of the peptides we must keep track of, in order
+    N = len(protein_sequence)
+    startpoints = []
+    endpoints = []
+    # initial peptides
+    for i in range(7, 15):
+        for j in range(i - 7 + 1):
+            startpoints.append(j)
+            endpoints.append(i)
+    for i in range(15, N):
+        for j in range(i - 15, i - 6):
+            startpoints.append(j)
+            endpoints.append(i)
+
+    lengths = [e - s + 1 for s, e in zip(startpoints, endpoints)]
+
+    return startpoints, endpoints, lengths
+
+
+def out_to_in(out_rates: np.ndarray):
+    # This function will take catalytic rates corresponding to ERAP1 and cytosolic aminopeptidases for the peptides in the system
+    # It will return a new array containing these rates rearranged to so that the precursor is in the corresponding index
+    in_rates = np.zeros_like(out_rates)
+    N = int(len(out_rates) / 9 + 11)
+    for i in range(7, 15):
+        start = int(0.5 * (i - 7) * (i - 6))
+        for j in range(1, i - 7 + 1):
+            in_rates[start + j] = out_rates[start + j - 1]
+    for i in range(15, N):
+        start = 36 + (i - 15) * 9
+        for j in range(1, 9):
+            in_rates[start + j] = out_rates[start + j - 1]
+    return in_rates
+
+
+def encode_tap_allele(alleles: np.ndarray) -> list:
+    allele_list = []
+    for allele in alleles:
+        if allele == "Human":
+            allele_list.append(HUMAN_TAP_PSEUDOSEQ)
+        elif allele == "Mouse":
+            allele_list.append(MOUSE_TAP_PSEUDOSEQ)
+        elif allele == "Rat_a":
+            allele_list.append(RAT_A_TAP_PSEUDOSEQ)
+        elif allele == "Rat_u":
+            allele_list.append(RAT_U_TAP_PSEUDOSEQ)
+    return allele_list
+
+
+def generate_tap_training_data(
+    startpoint: int,
+    encoding: str,
+    length: int,
+):
+    """_summary_
+
+    Args:
+        method (str): Whether to pad or to trim in order to get all peptides to the same length
+        startpoint (int): Which point in the peptide to start padding or trimming (N.B. corresponds to P_{i+1})
+        encoding (str): Which encoding method to use
+    """
+    train_data = pd.read_csv("poem/src/tap_dataset_processed.csv")
+
+    train_peptides = make_length(train_data.Peptide.values, startpoint, length)
+    if encoding not in VALID_ENCODINGS:
+        print("Uh oh", encoding)
+        raise ValueError("Must choose a method in VALID_ENCODINGS")
+
+    if encoding == "sparse":
+        train_peptide_encodings = sparse_peptide_encoding(train_peptides)
+        train_tap_encodings = sparse_peptide_encoding(
+            encode_tap_allele(train_data.Host.values)
+        )
+    elif encoding == "physico":
+        train_peptide_encodings = physico_peptide_encoding(train_peptides)
+        train_tap_encodings = physico_peptide_encoding(
+            encode_tap_allele(train_data.Host.values)
+        )
+    else:
+        train_peptide_encodings = submatrix_peptide_encoding(
+            train_peptides, encoding
+        )
+        train_tap_encodings = submatrix_peptide_encoding(
+            encode_tap_allele(train_data.Host.values), encoding
+        )
+
+    train_X = np.hstack([train_peptide_encodings, train_tap_encodings])
+
+    return (
+        train_X,
+        np.log10(train_data["IC50 (nM)"].values),
+        train_data.Length.values,
+    )
+
+
+def encode_tap_peptides(
+    peptides: np.ndarray,
+    hosts: np.ndarray,
+    startpoint: int,
+    encoding: str,
+    length: int,
+):
+    fixed_peptides = make_length(peptides, startpoint, length)
+
+    if encoding not in VALID_ENCODINGS:
+        raise ValueError("Must choose a method in VALID_ENCODINGS")
+
+    if encoding == "sparse":
+        peptide_encodings = sparse_peptide_encoding(fixed_peptides)
+        tap_encodings = sparse_peptide_encoding(encode_tap_allele(hosts))
+    elif encoding == "physico":
+        peptide_encodings = physico_peptide_encoding(fixed_peptides)
+        tap_encodings = physico_peptide_encoding(encode_tap_allele(hosts))
+    else:
+        peptide_encodings = submatrix_peptide_encoding(
+            fixed_peptides, encoding
+        )
+        tap_encodings = submatrix_peptide_encoding(
+            encode_tap_allele(hosts), encoding
+        )
+
+    X = np.hstack([peptide_encodings, tap_encodings])
+
+    return X
+
+
+# NETMHCPAN_PSEUDOSEQUENCES = pd.read_csv(
+#     "lib/poem/netmhcpan_pseudo.dat", sep="\s+"
+# )
+# netmhcpan_dict = dict(
+#     zip(
+#         netmhcpan_pseudosequences["allele"],
+#         netmhcpan_pseudosequences["pseudosequence"],
+#     )
+# )
+# g_alphas = pd.read_csv("poem/src/g_alpha.tsv", delimiter="\t")
+# g_betas = pd.read_csv("poem/src/g_beta.tsv", delimiter="\t")
+
+# full_sequences = [
+#     g_alphas[g_alphas.mhc_allele == a].sequence.values[0].replace("-", "")
+#     + g_betas[g_betas.mhc_allele == a].sequence.values[0].replace("-", "")
+#     for a in g_alphas.mhc_allele.values[:13350]
+# ]
+# full_dict = dict(
+#     zip(
+#         [x.replace("*", "") for x in g_alphas.mhc_allele.values[:13350]],
+#         full_sequences,
+#     )
+# )
+
+
+# def mhci_allele_to_pseudosequence(allele: str):
+#     parsed_allele = mhcgnomes.parse(allele)
+#     # sometimes the compact names parse to serotypes, e.g. B3901. I am unsure why.
+#     if type(parsed_allele) is mhcgnomes.serotype.Serotype:
+#         parsed_allele = parsed_allele.alleles[0].to_string()
+#     else:
+#         parsed_allele = parsed_allele.to_string()
+#     return netmhcpan_dict[parsed_allele.replace("*", "")]
+
+
+# def allele_to_pseudovec(allele: str, enc: str = "BLOSUM50"):
+#     """Returns BLOSUM50 (or other) encoding of allele"""
+#     if enc == "sparse":
+#         return sparse_peptide_encoding([mhci_allele_to_pseudosequence(allele)])
+
+#     else:
+#         return submatrix_peptide_encoding(
+#             [mhci_allele_to_pseudosequence(allele)], matrix=enc
+#         )
+
+
+# def allele_to_whole_vec(allele: str, enc: str = "BLOSUM50"):
+#     """Returns BLOSUM50 (or other) encoding of allele"""
+#     parsed_allele = mhcgnomes.parse(allele)
+#     # sometimes the compact names parse to serotypes, e.g. B3901. I am unsure why.
+#     if type(parsed_allele) is mhcgnomes.serotype.Serotype:
+#         parsed_allele = parsed_allele.alleles[0].to_string()
+#     else:
+#         parsed_allele = parsed_allele.to_string()
+#     parsed_allele = parsed_allele.replace("*", "")
+#     if enc == "sparse":
+#         return sparse_peptides([full_dict[parsed_allele]])
+
+#     else:
+#         return encode_peptides([full_dict[parsed_allele]], matrix=enc)
